bc193b1826f2a0aaf542d94fdc8a4f9eee4cc02e
ryougifujino <ryougifujino@gmail.com>
2019-12-28 23:57:03 +0800

    Advanced Guides - Code-Splitting  - Route-based code splitting

---
+++ advanced-guides/code-splitting/index2.js
@@ -0,0 +1,18 @@
+/** Route-based code splitting */
+
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import React, {Suspense, lazy} from 'react';
+
+const Home = lazy(() => import('./routes/Home'));
+const About = lazy(() => import('./routes/About'));
+
+const App = () => (
+    <Router>
+        <Suspense fallback={<div>Loading...</div>}>
+            <Switch>
+                <Route exact path="/" component={Home}/>
+                <Route path="/about" component={About}/>
+            </Switch>
+        </Suspense>
+    </Router>
+);
