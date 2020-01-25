a5aa8fe91c4a2d938f86e0850e518ee27b574a3e
ryougifujino <ryougifujino@gmail.com>
2020-01-26 03:23:13 +0800

    Advanced Guides - Typechecking With PropTypes

---
+++ advanced-guides/typechecking-with-proptypes/index.js
@@ -0,0 +1,43 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+class Greeting extends React.Component{
+    render() {
+        return (
+            <h1>Hello, {this.props.name}</h1>
+        );
+    }
+}
+
+Greeting.propTypes = {
+    name: PropTypes.string,
+    // You can also specify a custom validator. It should return an Error
+    // object if the validation fails. Don't `console.warn` or throw, as this
+    // won't work inside `oneOfType`.
+    customProp: function(props, propName, componentName) {
+        if (!/matchme/.test(props[propName])) {
+            return new Error(
+                'Invalid prop `' + propName + '` supplied to' +
+                ' `' + componentName + '`. Validation failed.'
+            );
+        }
+    },
+
+    // You can also supply a custom validator to `arrayOf` and `objectOf`.
+    // It should return an Error object if the validation fails. The validator
+    // will be called for each key in the array or object. The first two
+    // arguments of the validator are the array or object itself, and the
+    // current item's key.
+    customArrayProp: PropTypes.arrayOf(function(propValue, key, componentName, location, propFullName) {
+        if (!/matchme/.test(propValue[key])) {
+            return new Error(
+                'Invalid prop `' + propFullName + '` supplied to' +
+                ' `' + componentName + '`. Validation failed.'
+            );
+        }
+    })
+};
+
+Greeting.defaultProps = {
+    name: 'Stranger'
+};
